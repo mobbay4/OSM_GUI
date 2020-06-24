327e271b241a2b38a2cc68a505201d719be95844
Martin Zaech <martin.zaech@siemens.com>
2020-06-24 13:14:31 +0200

    added comments to skripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#virtualenv --python=python3.7 venv
+#install virtual environment package -- just to be shure
 py -m pip install virtualenv
+#create a virtual environment in the application folder
 py -m virtualenv ./OSM_GUI_venv
+#install the required packages to the virtual environment
 py -m pip install -r requirements.txt
