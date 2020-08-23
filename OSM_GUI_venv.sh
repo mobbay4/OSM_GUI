116609507b7c3ebb92db91c80d43a2818875d9fe
Martin Zaech <martin.zaech@siemens.com>
2020-08-23 16:45:16 +0200

    changed creation of virtual environment to anaconda

---
+++ OSM_GUI_venv.sh
@@ -0,0 +1,51 @@
+# ###############################################################################################
+#
+#  This Script is to set up the packages for the environment "OSM_GUI_venv"
+#  It provieds a quick recreation of the virtual environment
+#    Location: C:\Anaconda3\envs\OSM_GUI_venv
+#    
+#  NOTE: - for adding new packages to the environment add them to the requierements
+#             --> (if there is a conda version --> requierements_conda.txt // otherwise requirements_pip.txt)
+#        - anaconda recoments to generate the environment new, if a conda package has to be added after a pip package
+#           https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#using-pip-in-an-environment
+#       
+#  
+# ###############################################################################################
+#
+# ###############################################################################################
+# handle virtual environment
+# ###############################################################################################
+#
+# - To create the environment use:
+#conda create --name "OSM_GUI_venv"   
+#
+# - To activate this environment, use
+#conda activate OSM_GUI_venv
+#
+# - To deactivate an active environment, use
+#conda deactivate
+#
+# - To delete the environment
+#conda remove --name OSM_GUI_venv --all 
+# - To verify that the environment was removed, in your terminal window or an Anaconda Prompt, run:
+#conda info --envs   
+#
+#
+# ###############################################################################################
+# package section --> run requirements
+# ###############################################################################################
+#conda install --file requirements_conda.txt
+#pip install -r requirements_pip.txt
+#
+#
+# ###############################################################################################
+# create jupyter notebook kernel in this virtual environment 
+# ###############################################################################################
+#   manual: https://anbasile.github.io/programming/2017/06/25/jupyter-venv/
+#   --> ipython package installed with requirements_cond.txt
+#
+## check this line next time (with activated enviroment):
+#python -m ipykernel install
+## the old one has a problem with the "--user" parameter:
+## jupyter notebook cernel can not be started from VScode then
+# ipython kernel install --user --name=OSM_GUI_venv
